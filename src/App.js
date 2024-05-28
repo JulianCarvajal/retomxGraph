49f50cd77c9461d6bd3314c5727d77934db579f7
Julian Carvajal <julianc.carvajal12@gmail.com>
2024-05-27 20:04:17 -0500

    Algunos estilos básicos para que no se vea tan feo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import GraphEditor from './components/GraphEditor';
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>Reto mxGraph</h1>
+      </header>
       <GraphEditor />
     </div>
   );
--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -1,6 +1,7 @@
 // Componente donde se renderiza el graph
 // Es el "Lienzo" para trabajar
 import React, { forwardRef } from 'react';
+import './GraphContainer.css';
 
 const GraphContainer = forwardRef((props, ref) => {
   return <div className="graph-container" ref={ref}></div>;
