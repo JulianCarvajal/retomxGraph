7e810d5c1941b6cc5069cb61e273dd6ec774951d
Julian Carvajal <julianc.carvajal12@gmail.com>
2024-05-27 23:10:54 -0500

    Botón que permite exportar el archivo XML del graph.

--- src/components/XmlViewer.js
+++ src/components/XmlViewer.js
@@ -3,10 +3,22 @@ import React from 'react';
 import './XmlViewer.css';
 
 const XMLViewer = ({ xml }) => {
+
+  const handleExportXML = () => {
+    const blob = new Blob([xml], { type: 'text/xml' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'graph.xml';
+    a.click();
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <div className="xml-viewer">
       <h2 className="xml-title">Código XML</h2>
       <textarea readOnly value={xml}></textarea>
+      <button onClick={handleExportXML}>Exportar XML</button>
     </div>
   );
 };
